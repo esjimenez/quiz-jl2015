1495da7cc903f98f30ada6ac8863e2495adb1d23
esjimenez <ejimoli@gmail.com>
2015-07-11 18:02:18 +0000

    Primera pregunta

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-var router = require('./routes/index')
+var routes = require('./routes/index')
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
 
 var app = express();
 
