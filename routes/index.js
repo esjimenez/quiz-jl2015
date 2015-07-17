dfaed47320040ec68e22a1db7241b52d4701db8b
esjimenez <ejimoli@gmail.com>
2015-07-17 18:01:45 +0000

    Despliegue BD en Heroku

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+    res.render('index',{title: 'Quiz'});
 });
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-
-router.get('/author', function(req, res) {
-  res.render('author', { title: 'Créditos' });
-});
+// Definición de rutas
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
