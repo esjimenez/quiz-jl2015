c99b8dae69c23b46b2c8b5ff7d6af2bbb2756272
esjimenez <ejimoli@gmail.com>
2015-07-30 16:42:48 +0000

    Autoload

--- models/quiz.js
+++ models/quiz.js
@@ -1,7 +1,7 @@
 // Definicion del modelo de Quiz 
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('quiz',
+  return sequelize.define('Quiz',
     { pregunta: DataTypes.STRING,
       respuesta:DataTypes.STRING,
     }
