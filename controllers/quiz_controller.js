63dfce641428fd80908c49cf8b80d12092cc3eb0
esjimenez <ejimoli@gmail.com>
2015-07-31 17:09:14 +0000

    Ejercicio modulo7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,10 +14,17 @@ exports.load = function(req, res, next, quizId) {
 
 //GET /quizes
 exports.index = function(req, res) {
-    models.Quiz.findAll().then(function(quizes){
-        res.render('quizes/index.ejs', {quizes : quizes});
-    }
-    )
+    if(req.query.search) {
+		var filtro=(req.query.search||'').replace(' ', '%');
+ 		models.Quiz.findAll({where:['pregunta like ?','%'+filtro+'%'],order:'pregunta ASC'
+ 			}).then(function(quizes){
+				res.render('quizes/index', {quizes:quizes});
+			}).catch(function(error){next(error);});
+    } else {
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index', {quizes:quizes});
+		}).catch(function(error) {next(error);});
+	}
 };
 
 // GET /quizes/show
